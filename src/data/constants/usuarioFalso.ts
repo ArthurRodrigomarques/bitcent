4adf075937d2dc5487fef542ce2501eae446215e
ArthurRodrigomarques <ArthurRodrigomarques@users.noreply.github.com>
2023-05-13 22:08:51 -0300

    add

---
+++ src/data/constants/usuarioFalso.ts
@@ -0,0 +1,10 @@
+
+import Id from "@/logic/core/comum/Id";
+import Usuario from "@/logic/core/usuario/Usuario";
+
+export default {
+    id: Id.novo(),
+    nome: "Arthur Marques",
+    email: "aaaarthur@gmail.com",
+    imagemUrl: null,
+} as Usuario
---
+++ src/pages/usuario.tsx
@@ -0,0 +1,14 @@
+import Cabecalho from "@/components/template/Cabecalho";
+import Pagina from "@/components/template/Pagina";
+import Id from "@/logic/core/comum/id";
+
+export default function Home() {
+  return (
+    <Pagina>
+        <Cabecalho/>
+        <div>
+            Usuario 
+        </div>
+    </Pagina>
+  )
+}
